27b686b76653367acd56c68a69b31a25fe5cb403
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-08-09 15:11:07 +0300

    fix: fix input component adaptation

--- src/components/styledInput/index.ts
+++ src/components/styledInput/index.ts
@@ -4,14 +4,16 @@ const StyledInput = styled.input<{ $maxWidth?: string }>`
   background: none;
   color: #cf844e;
   font-family: Arial;
+  font-size: 16px;
   padding: 10px 20px;
   margin: 25px 5px 0 5px;
   border: 1px solid #c4c4c4;
   border-radius: 7px;
   max-width: ${({ $maxWidth }) => ($maxWidth ? '630px' : $maxWidth)};
-  width: 100%;
+  width: auto;
   outline: 0;
   outline-offset: 0;
+
 
   &:invalid {
     border: solid 2px #e00000;
