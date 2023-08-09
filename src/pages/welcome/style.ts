9624c7f8ffe8c81d4c666958cff27b364699448b
Dmitry Mikheev <dimikh@mail.ru>
2023-08-09 14:26:56 +0300

    fix: install and configure lint-staged

--- src/pages/welcome/style.ts
+++ src/pages/welcome/style.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const StyledWelcomeWrapper = styled.div`
   display: block;
   margin: 0 auto;
-  width: fit-content;
 `;
 
 export default StyledWelcomeWrapper;
