f53fdd06e534aa3f1bb318b4770dd22d8856c1b5
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-08-09 14:10:40 +0300

    feat: add input component

---
+++ src/components/styledInput/index.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const StyledInput = styled.input<{ $maxWidth?: string }>`
+  background: none;
+  color: #cf844e;
+  font-family: Arial;
+  padding: 10px 20px;
+  margin: 25px 5px 0 5px;
+  border: 1px solid #c4c4c4;
+  border-radius: 7px;
+  max-width: ${({ $maxWidth }) => ($maxWidth ? '630px' : $maxWidth)};
+  width: 100%;
+  outline: 0;
+  outline-offset: 0;
+
+  &:invalid {
+    border: solid 2px #e00000;
+  }
+
+  &::placeholder {
+    color: #cf844e;
+  }
+`;
+
+export default StyledInput;
