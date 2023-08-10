5b32a7f0fda17e2a2aa725f82afb484b2ce2dde5
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-08-10 14:23:50 +0300

    feat: add styled input

--- src/components/styledInput/index.ts
+++ src/components/styledInput/index.ts
@@ -1,19 +1,18 @@
 import styled from 'styled-components';
 
-const StyledInput = styled.input<{ $maxWidth?: string }>`
+const StyledInput = styled.input<{ $margin?: string; $maxWidth?: string }>`
   background: none;
   color: #cf844e;
   font-family: Arial;
   font-size: 16px;
   padding: 10px 20px;
-  margin: 25px 5px 0 5px;
   border: 1px solid #c4c4c4;
   border-radius: 7px;
-  max-width: ${({ $maxWidth }) => ($maxWidth ? '630px' : $maxWidth)};
+  margin: ${({ $margin }) => (!$margin ? '0' : $margin)};
+  max-width: ${({ $maxWidth }) => (!$maxWidth ? '630px' : $maxWidth)};
   width: auto;
   outline: 0;
   outline-offset: 0;
-
 
   &:invalid {
     border: solid 2px #e00000;
