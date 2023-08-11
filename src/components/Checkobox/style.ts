787f3ae7531c0613d28485d146c47a9cadee713c
Dmitry Mikheev <dimikh@mail.ru>
2023-08-11 17:17:11 +0300

    feat: add styled checkbox

---
+++ src/components/Checkobox/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledLabel = styled.label`
+  font-size: 18px;
+`;
+
+export const StyledInput = styled.input`
+  width: 1.11em;
+  height: 1.11em;
+`;
