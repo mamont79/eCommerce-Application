715be368fd0b4344b642cd29b94ccdd7bb5cc029
Pavel Dalhou <dalhoupavel@gmail.com>
2023-08-13 22:12:15 +0200

    refactor: replace call of navbar from main to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-// import React from 'React';
 import { Routes, Route } from 'react-router-dom';
 import Welcome from './pages/welcome';
 import Login from './pages/login';
 import NotFound from './pages/notFound';
 import Registration from './pages/registration';
-
-// const navigate = useNavigate();
+import NavBar from './components/navBar';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Welcome />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/registration" element={<Registration />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
