31bdae683a304563242c941e4cde66d39da9b097
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-08-14 10:50:15 +0300

    feat: add styled error span

---
+++ src/components/styledErrorSpan/index.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledErrorSpan = styled.span`
+  color: #e00000;
+  font-family: Arial;
+  font-size: 12px;
+  margin: 5px 0 5px 0;
+`;
+
+export default StyledErrorSpan;
