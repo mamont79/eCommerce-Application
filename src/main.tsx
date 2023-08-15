606bb50a06341880beedf5130e47df64bf256d5f
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-08-15 10:01:30 +0300

    fix: move navbar from main to app

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,12 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import NavBar from './components/navBar';
 import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <NavBar />
         <App />
       </BrowserRouter>
     </Provider>
