4e64e47f2846e93dbfb743ffa03a922ba7ac7736
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-08-16 10:38:07 +0300

    refactor: remove comments

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-cycle */
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
-// import type { PayloadAction } from '@reduxjs/toolkit'
 import { UsersState } from './usersReducerTypes';
 import { RootState } from '../../store/store';
 
@@ -16,18 +15,6 @@ export const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {
-    // login: (state) => {
-    //   state.status = UserStatusTypes.LOADING;
-    // },
-    // registration: (state, action: PayloadAction<object>) => {
-    //   state.status = UserStatusTypes.LOADING;
-    //   state.user = action.payload;
-    // },
-    // logout: (state, action: PayloadAction<string>) => {
-    //   state.status = UserStatusTypes.LOADING;
-    //   state.message = action.payload;
-    // },
-
     increment: (state) => {
       state.value += 1;
     },
@@ -39,7 +26,6 @@ export const usersSlice = createSlice({
 
 export const { increment, decrement } = usersSlice.actions;
 
-// export const selectUser = (state: RootState) => state.users.user
 export const selectValue = (state: RootState) => state.users.value;
 
 export default usersSlice.reducer;
