6907821a1c9a70ac7f0e7d75a482543c95c5b3bf
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-08-17 18:06:32 +0300

    feat: add login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,48 @@
-import { StyledBtn, StyledCheckBtn } from '../../components/styledBtn';
-import WelcomeWrapper from './style';
+import { Formik } from 'formik';
+import {
+  LoginBtn,
+  LoginInputWrapper,
+  LoginLink,
+  StyledLoginForm,
+} from './style';
+import EmailFormInput from '../../components/formInputs/emailFormInput';
+import PasswordFormInput from '../../components/formInputs/passwordFormInput';
+import StyledErrorMessage from '../../components/errorMessage/styledErrorMessage';
 
 export default function Login() {
   return (
-    <WelcomeWrapper>
-      <h1>Login</h1>
-      <StyledBtn>Open</StyledBtn>
-      <StyledBtn $primary>Close</StyledBtn>
-      <StyledCheckBtn>Check</StyledCheckBtn>
-    </WelcomeWrapper>
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      onSubmit={(values) => {
+        // eslint-disable-next-line no-console
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <StyledLoginForm>
+          <h2>Glad to see you!</h2>
+          <EmailFormInput />
+          {errors.email && touched.email && (
+            <StyledErrorMessage>{errors.email}</StyledErrorMessage>
+          )}
+          <LoginInputWrapper>
+            <PasswordFormInput />
+          </LoginInputWrapper>
+          {errors.password && touched.password && (
+            <StyledErrorMessage>{errors.password}</StyledErrorMessage>
+          )}
+          <LoginLink to="1">Forgot your password?</LoginLink>
+          <LoginBtn primary type="submit">
+            Log in
+          </LoginBtn>
+          <LoginLink to="/registration">
+            Don&apos;t have an account yet? Sign up
+          </LoginLink>
+        </StyledLoginForm>
+      )}
+    </Formik>
   );
 }
