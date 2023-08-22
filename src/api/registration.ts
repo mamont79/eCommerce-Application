680337e5726703bde61b2592e5316be72f22313e
Pavel Dalhou <dalhoupavel@gmail.com>
2023-08-22 08:24:24 +0200

    refactor: change type of syntax for registration

--- src/api/registration.ts
+++ src/api/registration.ts
@@ -1,29 +1,24 @@
-/* eslint-disable no-console */
 import { getTokenCookie } from './cookieToken';
 import { axiosInstance } from './index';
 
-// eslint-disable-next-line consistent-return
 export const registrationCustomer = async () => {
-  try {
-    const authToken = getTokenCookie('access_token');
-    console.log(authToken);
+  const authToken = getTokenCookie('access_token');
 
-    const response = await axiosInstance({
-      method: 'post',
-      url: '/me/signup',
+  const response = await axiosInstance.post(
+    '/me/signup',
+    {
+      email: 'billy-cl@rambler.ru',
+      firstName: 'Bill',
+      lastName: 'Clinton',
+      password: 'Bill!Clinton99',
+    },
+    {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${authToken}`,
       },
-      data: {
-        email: 'billy@rambler.ru',
-        firstName: 'Bill',
-        lastName: 'Clinton',
-        password: 'Bill!Clinton99',
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+    }
+  );
+
+  return response.data;
 };
