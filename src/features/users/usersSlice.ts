dfe40aee29d34f757a5c650d5e468704aef1d071
Dmitry Mikheev <dimikh@mail.ru>
2023-08-23 10:01:07 +0300

    feat: Integrate the registration with auth

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -8,6 +8,7 @@ import { registrationCustomer } from '../../api/registration';
 import { loginMeCustomer } from '../../api/login';
 import { LoginData } from '../../api/authTypes';
 import { deleteMailToken } from '../../api/cookieToken';
+import { INewUserData } from '../../pages/registration/RegistrationForm/CustomFormElements/requestTypes';
 
 const initialState: UsersState = {
   user: null,
@@ -48,10 +49,22 @@ export const fetchAuthEmailToken = createAsyncThunk(
 
 export const fetchRegisterCustomer = createAsyncThunk(
   'users/fetchRegisterCustomer',
-  async () => {
-    const response = await registrationCustomer();
-    // eslint-disable-next-line no-console
-    console.log(response);
+  async (newUserData: INewUserData, thunkAPI) => {
+    let response = null;
+    try {
+      response = await registrationCustomer(newUserData);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        return thunkAPI.rejectWithValue(message);
+      }
+    }
+    return response;
   }
 );
 
@@ -98,6 +111,18 @@ export const usersSlice = createSlice({
         state.user = action.payload;
       })
       .addCase(fetchLoginMeCustomer.rejected, (state, action) => {
+        state.status = UserStatusTypes.ERROR;
+        state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(fetchRegisterCustomer.pending, (state) => {
+        state.status = UserStatusTypes.LOADING;
+      })
+      .addCase(fetchRegisterCustomer.fulfilled, (state, action) => {
+        state.status = UserStatusTypes.SUCCESS;
+        state.user = action.payload;
+      })
+      .addCase(fetchRegisterCustomer.rejected, (state, action) => {
         state.status = UserStatusTypes.ERROR;
         state.message = action.payload;
         state.user = null;
