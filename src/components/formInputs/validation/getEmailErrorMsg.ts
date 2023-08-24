6579d80a354b6ae5a27968976469df8cf95ad325
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-08-24 11:07:28 +0300

    fix: convert email to lower case

--- src/components/formInputs/validation/getEmailErrorMsg.ts
+++ src/components/formInputs/validation/getEmailErrorMsg.ts
@@ -8,7 +8,7 @@ function getEmailErrorMsg(value: string): string {
   if (checkSpaces(value)) {
     return `e-mail address must not contain leading or trailing whitespace.`;
   }
-  if (!EMAIL_SEPARATORS_CHECK.test(value)) {
+  if (!EMAIL_SEPARATORS_CHECK.test(value.toLowerCase())) {
     return `e-mail address must contain an '@' and domain name.`;
   }
 
