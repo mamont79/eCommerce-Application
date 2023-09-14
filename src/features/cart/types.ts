1f91f79b2cd9eb7ca27b3655a11e7148d4ff68d5
Dmitry Mikheev <dimikh@mail.ru>
2023-09-14 17:21:26 +0300

    refactor: change cart state update logic

--- src/features/cart/types.ts
+++ src/features/cart/types.ts
@@ -4,7 +4,5 @@ import { ICartFields } from '../../pages/basket/types';
 export type ICartState = {
   cart: Cart | null;
   cartFields: ICartFields | null;
-  status: string | null;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  message: any | null;
+  message: string | null;
 };
