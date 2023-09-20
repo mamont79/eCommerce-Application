e263fba64f17cdbfea6314d6c032e6f773b38573
Pavel Dalhou <dalhoupavel@gmail.com>
2023-09-20 21:57:56 +0200

    refactor: change catalog page style

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -5,35 +5,23 @@ import { useLocation } from 'react-router-dom';
 import {
   fetchAllCategories,
   fetchCatalog,
-  fetchCategory,
   resetProducts,
 } from '../../features/products/productsSlice';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import Card from '../../components/card/card';
 import { Product } from '../../components/card/types';
-import {
-  StyledCardsWrapper,
-  StyledCatalogFilterBar,
-  StyledCatalogWrapper,
-  StyledCategoryButtonWrapper,
-} from './style';
-import { StyledCardBtn } from '../../components/card/style';
-import { Category } from '../../features/products/productsType';
+import { StyledCardsWrapper, StyledCatalogWrapper } from './style';
 
 export default function Catalog() {
   const dispatch = useAppDispatch();
   const location = useLocation();
 
-  const [categoryId, setCategoryId] = useState<string | null>(null);
   const [fetching, setFetching] = useState<boolean>(true);
 
   const cardsData = useAppSelector((state) => state.products.cardData);
-  const categoriesData = useAppSelector((state) => state.products.categories);
   const catalogCurrentPage = useAppSelector(
     (state) => state.products.catalogCurrentPage
   );
-
-  const getCurrentId = (id: string) => () => setCategoryId(id);
 
   function scrollHandler(): void {
     if (
@@ -68,24 +56,8 @@ export default function Catalog() {
     };
   }, []);
 
-  useEffect(() => {
-    if (categoryId) {
-      dispatch(fetchCategory(categoryId));
-    }
-  }, [categoryId]);
-
   return (
     <StyledCatalogWrapper>
-      <StyledCatalogFilterBar>
-        {categoriesData.map(({ key, id }: Category) => (
-          <StyledCategoryButtonWrapper key={key}>
-            <StyledCardBtn $primary onClick={getCurrentId(id)}>
-              {key.split('-').reverse().join(' ').toUpperCase()}
-            </StyledCardBtn>
-          </StyledCategoryButtonWrapper>
-        ))}
-      </StyledCatalogFilterBar>
-
       <StyledCardsWrapper>
         {cardsData.map((product: Product) => (
           <Card product={product} key={product.id} />
