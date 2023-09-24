e96654f152bf027e31ecfb9cef2e76aae53d5240
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-09-24 21:11:17 +0300

    refactor: map header links

--- src/components/navBar/FirstHeaderRow/navigationLinks.tsx
+++ src/components/navBar/FirstHeaderRow/navigationLinks.tsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 import BasketSvg from '../../../assets/basketIcon';
 import DoorSvg from '../../../assets/doorIcon';
 import { useAppSelector } from '../../../store/hooks';
-import { CustomerProfileLink } from './CustomerProfileLink';
 import LazzoneIconSvg from '../../../assets/lazzoneIcon';
 import {
+  StyleMenuLink,
   StyledFirstHeaderRow,
   StyledMenuLinkContainer,
   StyledMenuLinkLabel,
@@ -13,12 +13,39 @@ import {
 } from '../style';
 import { StyledCenteredContentContainer } from '../../contentContainer/contentContainer';
 import TeamIconSvg from '../../../assets/teamIcon';
+import UserIconSvg from '../../../assets/userIcon';
 
 export default function NavigationLinks() {
   const { user } = useAppSelector((state) => state.users);
   const [route, routeLabel] = !user
     ? ['/login', 'Login']
     : ['/logout', 'Logout'];
+  const menuLinks = [
+    {
+      link: '/basket',
+      IconTag: <BasketSvg color="#FFA555" />,
+      label: 'Cart',
+      visibility: true,
+    },
+    {
+      link: '/aboutus',
+      IconTag: <TeamIconSvg color="#FFA555" />,
+      label: 'About Us',
+      visibility: true,
+    },
+    {
+      link: '/profile',
+      IconTag: <UserIconSvg color="#FFA555" />,
+      label: 'Account',
+      visibility: Boolean(user),
+    },
+    {
+      link: route,
+      IconTag: <DoorSvg color="#FFA555" />,
+      label: routeLabel,
+      visibility: true,
+    },
+  ];
   return (
     <StyledFirstHeaderRow>
       <StyledCenteredContentContainer>
@@ -26,25 +53,14 @@ export default function NavigationLinks() {
           <LazzoneIconSvg />
         </Link>
         <StyledNavigation>
-          <Link to="/basket">
-            <StyledMenuLinkContainer>
-              <BasketSvg color="#FFA555" />
-              <StyledMenuLinkLabel>Cart</StyledMenuLinkLabel>
+          {menuLinks.map(({ link, IconTag, label, visibility }) => (
+            <StyledMenuLinkContainer key={label} $isVisible={visibility}>
+              <StyleMenuLink to={link}>
+                {IconTag}
+                <StyledMenuLinkLabel>{label}</StyledMenuLinkLabel>
+              </StyleMenuLink>
             </StyledMenuLinkContainer>
-          </Link>
-          <Link to="/aboutus">
-            <StyledMenuLinkContainer>
-              <TeamIconSvg color="#FFA555" />
-              <StyledMenuLinkLabel>About Us</StyledMenuLinkLabel>
-            </StyledMenuLinkContainer>
-          </Link>
-          <CustomerProfileLink isVisible={Boolean(user)} />
-          <Link to={route}>
-            <StyledMenuLinkContainer>
-              <DoorSvg color="#FFA555" />
-              <StyledMenuLinkLabel>{routeLabel}</StyledMenuLinkLabel>
-            </StyledMenuLinkContainer>
-          </Link>
+          ))}
         </StyledNavigation>
       </StyledCenteredContentContainer>
     </StyledFirstHeaderRow>
