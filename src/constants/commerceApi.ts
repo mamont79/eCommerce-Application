d58da11a223b11105dbfaae22a8971740d40c472
Pavel Dalhou <dalhoupavel@gmail.com>
2023-10-03 13:20:48 +0200

    feat: reanimate connection to commerceTools

--- src/constants/commerceApi.ts
+++ src/constants/commerceApi.ts
@@ -1,21 +1,21 @@
 export const credentials = 'after-dranik';
 export const projectKey = 'after-dranik';
-export const clientId = 'MDnuFO_5C_g2FLT0xU5mY2jd';
-export const secret = 'PSq48LMBI_dGOF1bS4n8w_krba8shzYx';
+export const clientId = 'cFgcYqA8Kg8nHh5xdgrzUdJO';
+export const secret = 'ymblLVtqmjV3PpHa5TRd2GoCv6ULH8mm';
 export const apiUrl = 'https://api.europe-west1.gcp.commercetools.com';
 export const authUrl = 'https://auth.europe-west1.gcp.commercetools.com';
 
-// export const CTP_PROJECT_KEY = 'cuber-dranik-ecommerce';
-// export const CTP_CLIENT_SECRET = 'cX0r3zRiHI1c1w1yBCXQAdS8X1NJ0_tS';
-// export const CTP_CLIENT_ID = 'J5huPXjl_icdfcvIMBrxKRH9';
+// export const CTP_PROJECT_KEY = 'after-dranik';
+// export const CTP_CLIENT_SECRET = 'PSq48LMBI_dGOF1bS4n8w_krba8shzYx';
+// export const CTP_CLIENT_ID = 'MDnuFO_5C_g2FLT0xU5mY2jd';
 // export const CTP_AUTH_URL = 'https://auth.europe-west1.gcp.commercetools.com';
 // export const CTP_API_URL = 'https://api.europe-west1.gcp.commercetools.com';
-// export const CTP_SCOPES =
-//   'manage_shopping_lists:cuber-dranik-ecommerce manage_extensions:cuber-dranik-ecommerce manage_categories:cuber-dranik-ecommerce manage_products:cuber-dranik-ecommerce manage_stores:cuber-dranik-ecommerce manage_my_orders:cuber-dranik-ecommerce manage_my_shopping_lists:cuber-dranik-ecommerce manage_orders:cuber-dranik-ecommerce manage_shipping_methods:cuber-dranik-ecommerce manage_product_selections:cuber-dranik-ecommerce manage_standalone_prices:cuber-dranik-ecommerce view_api_clients:cuber-dranik-ecommerce manage_my_quotes:cuber-dranik-ecommerce manage_subscriptions:cuber-dranik-ecommerce create_anonymous_token:cuber-dranik-ecommerce manage_customers:cuber-dranik-ecommerce manage_my_business_units:cuber-dranik-ecommerce manage_discount_codes:cuber-dranik-ecommerce manage_my_payments:cuber-dranik-ecommerce manage_states:cuber-dranik-ecommerce view_published_products:cuber-dranik-ecommerce manage_payments:cuber-dranik-ecommerce manage_my_quote_requests:cuber-dranik-ecommerce manage_order_edits:cuber-dranik-ecommerce manage_associate_roles:cuber-dranik-ecommerce manage_cart_discounts:cuber-dranik-ecommerce manage_my_profile:cuber-dranik-ecommerce introspect_oauth_tokens:cuber-dranik-ecommerce view_categories:cuber-dranik-ecommerce';
+// export const CTP_SCOPES = 'manage_project:after-dranik';
 
 export const CTP_PROJECT_KEY = 'after-dranik';
-export const CTP_CLIENT_SECRET = 'PSq48LMBI_dGOF1bS4n8w_krba8shzYx';
-export const CTP_CLIENT_ID = 'MDnuFO_5C_g2FLT0xU5mY2jd';
+export const CTP_CLIENT_SECRET = 'ymblLVtqmjV3PpHa5TRd2GoCv6ULH8mm';
+export const CTP_CLIENT_ID = 'cFgcYqA8Kg8nHh5xdgrzUdJO';
 export const CTP_AUTH_URL = 'https://auth.europe-west1.gcp.commercetools.com';
 export const CTP_API_URL = 'https://api.europe-west1.gcp.commercetools.com';
-export const CTP_SCOPES = 'manage_project:after-dranik';
+export const CTP_SCOPES =
+  'create_anonymous_token:after-dranik manage_my_payments:after-dranik view_cart_discounts:after-dranik manage_staged_quotes:after-dranik manage_states:after-dranik manage_shipping_methods:after-dranik manage_discount_codes:after-dranik manage_orders:after-dranik view_states:after-dranik view_published_products:after-dranik manage_quotes:after-dranik manage_payments:after-dranik manage_api_clients:after-dranik view_tax_categories:after-dranik manage_my_shopping_lists:after-dranik view_quotes:after-dranik manage_product_selections:after-dranik view_stores:after-dranik view_project_settings:after-dranik view_quote_requests:after-dranik manage_standalone_prices:after-dranik view_shipping_methods:after-dranik view_shopping_lists:after-dranik manage_types:after-dranik manage_subscriptions:after-dranik manage_customer_groups:after-dranik manage_stores:after-dranik manage_import_containers:after-dranik manage_my_orders:after-dranik manage_tax_categories:after-dranik manage_categories:after-dranik manage_extensions:after-dranik manage_cart_discounts:after-dranik manage_shopping_lists:after-dranik manage_products:after-dranik manage_quote_requests:after-dranik manage_project_settings:after-dranik manage_my_profile:after-dranik manage_my_quote_requests:after-dranik view_import_containers:after-dranik manage_order_edits:after-dranik view_standalone_prices:after-dranik view_customers:after-dranik introspect_oauth_tokens:after-dranik view_types:after-dranik manage_customers:after-dranik view_staged_quotes:after-dranik manage_my_quotes:after-dranik view_discount_codes:after-dranik view_customer_groups:after-dranik view_categories:after-dranik manage_my_business_units:after-dranik';
